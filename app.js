251179dee28ddc4305540d1fcf6429931cdb707c
s-r-aman <s.r.aman.sra@gmail.com>
2017-12-02 17:23:17 +0530

    Initial Commit.

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+
+// app.use((req, res, next) => {
+//     var now = new Date().toString();
+//     let log = `${now}: ${req.method} ${req.url}`;
+//     console.log(log);
+//     fs.appendFileSync('log.txt', log);
+//     next();
+// });
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFileSync("log.txt", log);
+//   next();
+  res.render('maintainence.hbs');  
+});
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('currentYear',() => {
+    return (new Date()).getFullYear();
+});
+
+hbs.registerHelper('title',() => {
+    return "Server";
+});
+
+hbs.registerHelper('screamIt',(data) => {
+    return data.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs',{
+        pageTitle: 'Server | Home Page',
+        // currentYear: (new Date()).getFullYear(),
+        message: 'Welcome to the server Home page'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        // currentYear: (new Date()).getFullYear(),
+        message: 'Welcome to the about page!'
+    });
+});
+
+app.get("/bad", (req, res) => {
+  res.send({
+      error: 'This page does not exists! '
+  });
+});
+
+app.listen(3000);
